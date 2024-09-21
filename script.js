7a14c636926c9ed13dea7aef9bd2d05a51a74a8d
Robson Lourenço <125693025+robsonllourenco@users.noreply.github.com>
2024-09-21 13:20:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('whatsapp-btn').addEventListener('click', function () {
+    window.open('https://chat.whatsapp.com/E881ms2eXPvCRN1wS3YtQf', '_blank');
+});
+
+document.getElementById('telegram-btn').addEventListener('click', function () {
+    window.open('https://t.me/ofertasepromoshopeee', '_blank');
+});
